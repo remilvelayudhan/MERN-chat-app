e58ce8aa631b518e101f81c5a4973c425b18aded
cvremil1 <remil.cv@infolitz.com>
2023-08-31 17:25:14 +0530

    chat area design compled

---
+++ client/src/components/otherMessage/OtherMessage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './otherMessage.css';
+
+const OtherMessage = () => {
+  return (
+    <div>OtherMessage</div>
+  )
+}
+
+export default OtherMessage
---
+++ client/src/components/selfMessage/SelfMessage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import  './selfMessage.css'
+
+const SelfMessage = () => {
+  return (
+    <div className='chat'>
+         <div className='con-icon'>{'R'}</div>
+            <div className='con-name'>{'remil'}</div>
+            <div className='con-lastmessage'>{'last message'}</div>
+            <div className='con-delete'>heloo</div>
+    </div>
+  )
+}
+
+export default SelfMessage
